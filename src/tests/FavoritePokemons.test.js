6141de78bd0378aa30223fa7713e67832e133995
Leandro Bonfim <leandrobonfim15@homtial.com>
2022-03-22 16:38:00 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { FavoritePokemons } from '../components';
+import data from '../data';
+import renderWithRouter from './helpers/renderWithRouter';
+
+describe('FavoritesPokemons component', () => {
+  test(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+  se a pessoa não tiver pokémons favoritos.`,
+  () => {
+    renderWithRouter(<FavoritePokemons />);
+    screen.getByText(/No favorite pokemon found/i);
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados. ', () => {
+    const positionStart = 3;
+    const positionEnd = 6;
+    const pokemons = data.slice(positionStart, positionEnd);
+    console.log(pokemons);
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    screen.getByText(/ekans/i);
+    screen.getByText(/Alakazam/i);
+    screen.getByText(/Mew/i);
+    const psy = screen.getAllByText(/Psychic/i);
+    expect(psy.length).toBe(2);
+    const poison = screen.getAllByText(/Poison/i);
+    expect(poison.length).toBe(1);
+  });
+});
